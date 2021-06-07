22742a000baead0d8a0f24a380b8291bd19af436
Ruslan Igorevich <r.borodavkin@eteron.ru>
2021-06-07 21:53:37 +0300

    add models

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const sequelize = require('./db');
+const models = require('./models/models');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
 const start = async () => {
     try {
         await sequelize.authenticate();
