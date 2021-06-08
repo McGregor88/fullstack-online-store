bd5ec56cb8255389f2c6c08ebc5408bace4350ec
Ruslan Igorevich <r.borodavkin@eteron.ru>
2021-06-08 18:43:17 +0300

    add ApiError

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const sequelize = require('./db');
 const models = require('./models/models');
 const router = require('./routes');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -12,6 +13,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api', router);
+// Обработка ошибок, идет в конце
+app.use(errorHandler);
 
 const start = async () => {
     try {
