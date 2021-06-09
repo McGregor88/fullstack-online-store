97ae8bd6c0d8bf00ccad7cdca4d3265d159f0dda
Ruslan Igorevich <r.borodavkin@eteron.ru>
2021-06-09 22:33:50 +0300

    add login, registration, auth

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,11 @@
 const Router = require('express');
+
 const userController = require('../controllers/userController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 const router = new Router();
 
-router.get('/auth', userController.auth);
+router.get('/auth', authMiddleware, userController.auth);
 router.post('/registration', userController.registration);
 router.post('/login', userController.login);
 
