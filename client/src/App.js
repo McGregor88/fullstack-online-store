96f606117800cc8e6a742f6b2e39a69b6d31cb90
Ruslan Igorevich <r.borodavkin@eteron.ru>
2021-06-11 00:08:29 +0300

    add initial pages, components, routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,15 @@
-function App() {
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import AppRouter from './components/AppRouter';
+
+const App = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <BrowserRouter>
+            <AppRouter />
+        </BrowserRouter>
     );
-}
+};
 
 export default App;
