a7573ee77161f0182625846a5a6e0371410b2d29
Ruslan Igorevich <r.borodavkin@eteron.ru>
2021-06-12 19:02:51 +0300

    update auth page

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import { Container, Card, Form, Button } from 'react-bootstrap';
+
+import './Auth.css';
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts';
 
 const Auth = () => {
+    const location = useLocation();
+    const isLogin = location.pathname === LOGIN_ROUTE;
+
     return (
-        <div>
-            Auth
-        </div>
+        <Container 
+            className="d-flex justify-content-center align-items-center auth-container"
+        >
+            <Card className="p-5 auth-card">
+                <h2 className="ml-auto mr-auto mb-5">{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
+                <Form className="d-flex flex-column">
+                    <Form.Control
+                        placeholder="Введите ваш email"
+                    />
+
+                    <Form.Control
+                        placeholder="Введите ваш пароль..."
+                        className="mt-3"
+                    />
+                    <Button
+                        variant="primary"
+                        className="mt-3"
+                    >
+                        {isLogin ? 'Войти' : 'Зарегистрироваться'}
+                    </Button>
+                    {isLogin ?
+                        <Form.Text>
+                            Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся!</NavLink>
+                        </Form.Text>
+                        :
+                        <Form.Text>
+                            Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войдите!</NavLink>
+                        </Form.Text>
+                    }
+                </Form>
+            </Card>
+        </Container>
     );
 };
 
