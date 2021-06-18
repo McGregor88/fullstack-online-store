21b9d7d9b51d8e1407ab8d1a0d193cab3e163aaf
Ruslan Igorevich <r.borodavkin@eteron.ru>
2021-06-18 21:53:31 +0300

    routes fix

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,9 @@
-const Router = require('express');
+const router = require('express').Router();
 
 const userRouter = require('./userRouter');
 const typeRouter = require('./typeRouter');
 const brandRouter = require('./brandRouter');
 const deviceRouter = require('./deviceRouter');
-
-const router = new Router();
 
 router.use('/user', userRouter);
 router.use('/type', typeRouter);
